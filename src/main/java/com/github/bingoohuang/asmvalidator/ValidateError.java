80d325805e68674264209a717336fe0cabccddc5
bingoohuang <bingoo.huang@gmail.com>
2016-12-02 17:26:14 +0800

    add getter for ValidateError

--- src/main/java/com/github/bingoohuang/asmvalidator/ValidateError.java
+++ src/main/java/com/github/bingoohuang/asmvalidator/ValidateError.java
@@ -1,14 +1,15 @@
 package com.github.bingoohuang.asmvalidator;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 
 @ToString @AllArgsConstructor
 public class ValidateError {
-    private String fieldName;
-    private final Object fieldValue;
-    private final String errorMessage;
+    @Getter String fieldName;
+    @Getter final Object fieldValue;
+    @Getter final String errorMessage;
 
     public ValidateError replaceFieldName(String oldName, String newName) {
         if (StringUtils.equals(oldName, fieldName)) fieldName = newName;
